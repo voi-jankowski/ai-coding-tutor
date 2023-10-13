5122a5bb281af3514fb4972a368e7145d6ab93b5
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-10-13 17:50:56 +1100

    Created intit function with inquirer to get user prompts for promptFunc.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // dependencies
 const { OpenAI } = require("langchain/llms/openai");
+const inquirer = require("inquirer");
 require("dotenv").config();
 
 // Creates and stores a wrapper for the OpenAI package along with basic configuration
@@ -10,3 +11,31 @@ const model = new OpenAI({
 });
 
 console.log({ model });
+
+// Uses the instantiated OpenAI wrapper, model, and makes a call based on input from inquirer
+const promptFunc = async (input) => {
+  try {
+    const res = await model.call(input);
+    console.log(res);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Initialization function that uses inquirer to prompt the user and returns a promise. It takes the user input and passes it through the call method
+const init = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Ask a coding question:",
+      },
+    ])
+    .then((inquirerResponse) => {
+      promptFunc(inquirerResponse.name);
+    });
+};
+
+// Calls the initialization function and starts the script
+init();
